73e63514b2a1d10fef4821b476e3d3d221ce8464
ronaldoss8 <ronaldosssantos1@gmail.coml>
2024-12-27 20:28:54 -0300

    TRcommit2

--- src/main/java/backfirststeps/Main.java
+++ src/main/java/backfirststeps/Main.java
@@ -6,12 +6,9 @@ import backfirststeps.model.Itens;
 public class Main {
     public static void main(String[] args) {
 
-        Itens ibrupofeno= new Itens(2,"Ibrupofeno 200mg",40,"box");
-        Itens paracetamol = new Itens(2,"Paracetamol 100mg",20,"gota");
 
         DataBase dataBase = new DataBase();
-        dataBase.register(ibrupofeno);
-        dataBase.register(paracetamol);
+
     }
 
 }
